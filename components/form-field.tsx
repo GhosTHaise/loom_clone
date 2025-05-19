7c7ec8826e13dc4be6237c2aa04dfea0232aefea
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-05-19 20:23:04 +0300

    feat : formField select

--- components/form-field.tsx
+++ components/form-field.tsx
@@ -10,21 +10,42 @@ const FormField = ({
   as = "input",
   options = []
 }: FormFieldProps) => {
-  const InputToRender = ({ type } : { type: string }) => {
-  if (type === "textarea") {
-    return <textarea />
-  } else if (type === "select") {
-    return <select />
-  } else {
-    return <input />
+  const InputToRender = ({ type }: { type: string }) => {
+    if (type === "textarea") {
+      return <textarea
+        id={id}
+        name={id}
+        value={value}
+        onChange={onChange}
+      />
+    } else if (type === "select") {
+      return <select
+        id={id}
+        name={id}
+        value={value}
+        onChange={onChange}
+      >
+        {
+          options.map(({label , value}) => (
+            <option key={label} value={value}>{label}</option>
+          ))
+        }
+      </select>
+    } else {
+      return <input
+        id={id}
+        name={id}
+        value={value}
+        onChange={onChange}
+      />
+    }
   }
-}
-return (
-  <div className='form-field'>
-    <label htmlFor={id}>{label}</label>
-    <InputToRender type={as} />
-  </div>
-)
+  return (
+    <div className='form-field'>
+      <label htmlFor={id}>{label}</label>
+      <InputToRender type={as} />
+    </div>
+  )
 }
 
 export default FormField
