a5a2b12bb9a49cdf39974ad34f517a817af7debf
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-05-25 19:57:11 +0300

    feat : validateWithArcjet

--- lib/actions/video.ts
+++ lib/actions/video.ts
@@ -7,6 +7,8 @@ import { BUNNY } from "@/constants";
 import { db } from "@/drizzle/db";
 import { videos } from "@/drizzle/schema";
 import { revalidatePath } from "next/cache";
+import aj from "../arcjet";
+import { fixedWindow, request } from "@arcjet/next";
 
 const VIDEO_STREAM_BASE_URL = BUNNY.STREAM_BASE_URL;
 const THUMBNAIL_STORAGE_BASE_URL = BUNNY.STORAGE_BASE_URL;
@@ -14,21 +16,36 @@ const THUMBNAIL_CDN_URL = BUNNY.CDN_URL;
 const BUNNY_LIBRARY_ID = getEnv("BUNNY_LIBRARY_ID");
 
 const ACCESS_KEYS = {
-    streamAccessKey : getEnv("BUNNY_STREAM_ACCESS_KEY"),
-    storageAccessKey : getEnv("BUNNY_STORAGE_ACCESS_KEY"),
+    streamAccessKey: getEnv("BUNNY_STREAM_ACCESS_KEY"),
+    storageAccessKey: getEnv("BUNNY_STORAGE_ACCESS_KEY"),
 };
 
 // Helper Functions
-const getSessionUserId = async () : Promise<string> => {
-    const session = await auth.api.getSession({ headers : await headers() });
+const getSessionUserId = async (): Promise<string> => {
+    const session = await auth.api.getSession({ headers: await headers() });
 
-    if(!session) throw new Error("Unauthenticated");
+    if (!session) throw new Error("Unauthenticated");
 
     return session.user.id;
 }
 
-const revalidatePaths = (paths : string[]) => {
+const revalidatePaths = (paths: string[]) => {
     paths.forEach((path) => revalidatePath(path));
+}
+
+const validateWithArcjet = async (fingerprint: string) => {
+    const rateLimit = aj.withRule(fixedWindow({
+        mode: "LIVE",
+        window: "1m",
+        max: 1,
+        characteristics: ['fingerprint']
+    }));
+
+    const req = await request();
+
+    const decision = await rateLimit.protect(req, fingerprint);
+
+    if (decision.isDenied()) throw new Error("Rate limit exceeded");
 }
 
 // Server Actions
@@ -38,11 +55,11 @@ export const getVideoUploadUrl = withErrorHandling(async () => {
     const videoResponse = await apiFetch<BunnyVideoResponse>(
         `${VIDEO_STREAM_BASE_URL}/${BUNNY_LIBRARY_ID}/videos`,
         {
-            method : "POST",
-            bunnyType : "stream",
-            body : {
-                title : "Temporary Title",
-                collectionId : ""
+            method: "POST",
+            bunnyType: "stream",
+            body: {
+                title: "Temporary Title",
+                collectionId: ""
             }
         }
     )
@@ -50,13 +67,13 @@ export const getVideoUploadUrl = withErrorHandling(async () => {
     const uploadUrl = `${VIDEO_STREAM_BASE_URL}/${BUNNY_LIBRARY_ID}/videos/${videoResponse.guid}`;
 
     return {
-        videoId : videoResponse.guid,
+        videoId: videoResponse.guid,
         uploadUrl,
-        accessKey : ACCESS_KEYS.streamAccessKey
+        accessKey: ACCESS_KEYS.streamAccessKey
     }
 })
 
-export const getThumbnailUploadUrl = withErrorHandling(async (videoId : string) => {
+export const getThumbnailUploadUrl = withErrorHandling(async (videoId: string) => {
     const fileName = `${Date.now()}-${videoId}-thumbnail`;
 
     const uploadUrl = `${THUMBNAIL_STORAGE_BASE_URL}/thumbnails/${fileName}`;
@@ -66,21 +83,23 @@ export const getThumbnailUploadUrl = withErrorHandling(async (videoId : string) 
     return {
         uploadUrl,
         cdnUrl,
-        accessKey : ACCESS_KEYS.storageAccessKey
+        accessKey: ACCESS_KEYS.storageAccessKey
     }
 })
 
-export const saveVideoDetails = withErrorHandling(async (videoDetails : VideoDetails)=> {
+export const saveVideoDetails = withErrorHandling(async (videoDetails: VideoDetails) => {
     const userId = await getSessionUserId();
+
+    await validateWithArcjet(userId);
 
     await apiFetch(
         `${VIDEO_STREAM_BASE_URL}/${BUNNY_LIBRARY_ID}/videos/${videoDetails.videoId}`,
         {
-            method : "POST",
-            bunnyType : "stream",
-            body : {
-                title : videoDetails.title,
-                description : videoDetails.description,
+            method: "POST",
+            bunnyType: "stream",
+            body: {
+                title: videoDetails.title,
+                description: videoDetails.description,
 
             }
         }
@@ -88,13 +107,13 @@ export const saveVideoDetails = withErrorHandling(async (videoDetails : VideoDet
 
     await db.insert(videos).values({
         ...videoDetails,
-        videoUrl : `${BUNNY.EMBED_URL}/${BUNNY_LIBRARY_ID}/${videoDetails.videoId}`,
+        videoUrl: `${BUNNY.EMBED_URL}/${BUNNY_LIBRARY_ID}/${videoDetails.videoId}`,
         userId,
-        createdAt : new Date(),
-        updatedAt : new Date()
+        createdAt: new Date(),
+        updatedAt: new Date()
     });
 
     revalidatePaths(["/"]);
 
-    return { videoId : videoDetails.videoId }
+    return { videoId: videoDetails.videoId }
 })
